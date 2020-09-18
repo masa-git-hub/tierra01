11de90d8dc8766c4337f2e68a64adf71fa33a397
masa-git-hub <62799243+masa-git-hub@users.noreply.github.com>
2020-09-18 17:52:00 +0900

    Update page-home.php

--- page-home.php
+++ page-home.php
@@ -42,26 +42,7 @@
 			</article>
 			<?php endif; ?>
 			
-			<div class="wow fadeIn" data-wow-delay="0.2s" data-wow-duration="2s">
-			<div class="profile_area"> 
-			<h2><?php the_field('profile_text'); ?></h2>
 			
-			<table> 
-			<?php while( have_rows('profile_loop') ): the_row(); ?>			
-			<tr>
-				<td class="profile_year"><?php the_sub_field('profile_year'); ?></td>
-				<td>
-					<ul>
-						<?php while( have_rows('profile_event_loop') ): the_row(); ?>
-						<li><?php the_sub_field('profile_event'); ?></li>
-						<?php endwhile; ?>
-					</ul>
-				</td>
-			</tr>
-			<?php endwhile; ?>
-			</table>
-			</div>
-			</div>
 		</main>
 	</div>
 	
